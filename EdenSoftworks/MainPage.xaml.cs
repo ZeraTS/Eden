b860f8fbf3e5f09eb99b681b8d03138faac99c38
Zera <51274450+ZeraTS@users.noreply.github.com>
2021-03-25 20:44:21 -0400

    Beta UI Release
    IP Geolocator Page + Main Page UI finished, C# code soon to be done.

---
+++ EdenSoftworks/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace EdenSoftworks
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+            ApplicationView.PreferredLaunchViewSize = new Size(640, 410);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(geoipwindow));
+        }
+
+        private void button7_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            scroller.Visibility = Visibility.Visible;
+            grid.Visibility = Visibility.Collapsed;
+        }
+    }
+}
---
+++ EdenSoftworks/geoipwindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using MUXC = Microsoft.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace EdenSoftworks
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class geoipwindow : Page
+    {
+        public geoipwindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void TabView_AddTabButtonClick(MUXC.TabView sender, object args)
+        {
+            var newTab = new MUXC.TabViewItem();
+            newTab.IconSource = new MUXC.SymbolIconSource() { Symbol = Symbol.Document };
+            newTab.Header = "New Document";
+
+            // The Content of a TabViewItem is often a frame which hosts a page.
+
+            sender.TabItems.Add(newTab);
+        }
+
+
+        private void TabView_TabCloseRequested(MUXC.TabView sender, MUXC.TabViewTabCloseRequestedEventArgs args)
+        {
+            sender.TabItems.Remove(args.Tab);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+
+        }
+
+        private void MenuFlyoutItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuFlyoutItem_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuFlyoutItem_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuFlyoutItem_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TabView_Loaded(object sender, RoutedEventArgs e)
+        {
+          
+        }
+    }
+}
